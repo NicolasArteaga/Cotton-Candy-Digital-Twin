141b12f1eb4f2c2a5720808b54075b77c9146eb2
Nicolas Arteaga <nico.arteaga99@gmail.com>
2025-07-04 15:07:22 +0200

    Update

--- manual_cc_data.py
+++ manual_cc_data.py
@@ -8,7 +8,7 @@ url = "https://lab.bpm.in.tum.de/sugarpi/"
 filename = datetime.now().strftime("%Y%m%d_%H%M%S") + "_sugarpi_data.csv"
 
 # Define the expected keys
-fields = ["timestamp", "EnvH", "EnvT", "InH", "InT", "IrA1", "IrO1", "IrA2", "IrO2"]
+fields = ["timestamp", "EnvH", "EnvT", "InH", "InT", "IrA", "IrO"]
 
 # Initialize CSV
 if not os.path.exists(filename):
@@ -21,7 +21,7 @@ while True:
     try:
         response = requests.get(url)
         data = response.json()
-        row = [data.get(k) for k in fields]
+        row = [f"{data.get(k):.2f}" if isinstance(data.get(k), (float, int)) else data.get(k) for k in fields]
 
         with open(filename, mode='a', newline='') as f:
             writer = csv.writer(f)
