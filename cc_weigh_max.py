129b1ff5fa0e9634fae3e49b165f2c94ebe00b9b
NicolasArteaga <nico.arteaga99@gmail.com>
2025-07-09 16:17:50 +0200

    Update

---
+++ cc_weigh_max.py
@@ -0,0 +1,26 @@
+from bottle import Bottle, run
+#import serial
+
+# ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None)
+
+app = Bottle()
+
+@app.route('/')
+def index():
+    return "TODO"
+
+# def weigh_touch():
+#     count = 0
+#     try:
+#         while True:
+#             recv = ser.readline().decode('utf-8', errors='ignore').strip()
+#             ser.reset_input_buffer()
+#             print(f"{count}: {recv}")
+#             count += 1
+#             return count
+#     except KeyboardInterrupt:
+#         ser.close()
+#         print("Serial port closed.")
+
+if __name__ == '__main__':
+    run(app, host='0.0.0.0', port=7203, debug=True)
--- restart.sh
+++ restart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo systemctl restart cc_environment.service
 sudo systemctl restart cc_weigh_touch.service
-#sudo systemctl restart energy.service
+sudo systemctl restart cc_weigh_max.service
+sudo systemctl restart cc_weigh_single.service
+
