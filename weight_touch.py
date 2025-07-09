a9a1ee81180d33b2284f5ae82e3195de3f3925f3
NicolasArteaga <nico.arteaga99@gmail.com>
2025-07-09 12:23:21 +0200

    Update

--- weight_touch.py
+++ weight_touch.py
@@ -22,4 +22,5 @@ def weigh_touch():
         ser.close()
         print("Serial port closed.")
 
-run(app, host='0.0.0.0', port=7202, debug=True)
+if __name__ == '__main__':
+    run(app, host='0.0.0.0', port=7202, debug=True)
