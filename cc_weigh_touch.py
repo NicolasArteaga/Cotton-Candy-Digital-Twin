94d0914f07d432c2cf527e777c5ac19b12771698
NicolasArteaga <nico.arteaga99@gmail.com>
2025-07-09 19:41:34 +0200

    Update

--- cc_weigh_touch.py
+++ cc_weigh_touch.py
@@ -6,16 +6,16 @@ app = Bottle()
 # Returns True if the weight is greater than or equal to 1.0 gram
 # Signaling that the scale has been touched
 def weigh_touch():
-    try:
-        ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None)
-        recv = ser.readline().decode('utf-8', errors='ignore').strip()
-        ser.reset_input_buffer()
-        try:
-            weight = float(recv)
-            return weight >= 1.0
-        except ValueError:
-            return False
-    except Exception:
+    # try:
+    #     ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None)
+    #     recv = ser.readline().decode('utf-8', errors='ignore').strip()
+    #     ser.reset_input_buffer()
+    #     try:
+    #         weight = float(recv)
+    #         return weight >= 1.0
+    #     except ValueError:
+    #         return False
+    # except Exception:
         return False
 
 
