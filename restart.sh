71cb1dfee30dd3d6fc04d9e59c740dcb693e9191
NicolasArteaga <nico.arteaga99@gmail.com>
2025-07-25 12:36:00 +0200

    Update

--- restart.sh
+++ restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo systemctl restart cc_environment.service
+sudo systemctl restart cc_plug.service
 sudo systemctl restart cc_weigh_touch.service
 sudo systemctl restart cc_weigh_max.service
 sudo systemctl restart cc_weigh_single.service
-
